328e78a6b3a464835bd07aed25658e64a1969b6d
BIJENPUNDIR <142763648+BIJENPUNDIR@users.noreply.github.com>
2026-08-29 08:48:13 +0530

    Add two-range Vizrt text box sizing script

---
+++ vizrt-scripts/text-sizing/two_range_text_box.vb
@@ -0,0 +1,39 @@
+dim LastText as String = ""
+
+sub OnExecPerField()
+
+    dim Data1Text as String
+    dim CharacterCount as Integer
+    dim BoxWidth as Integer
+    dim BoxHeight as Integer
+    dim TextPath as String
+
+    TextPath = "MAIN_SCENE*TREE*$object$TEXT"
+
+    ' Read the current Control Text value.
+    Data1Text = This.Geometry.Text
+    CharacterCount = Len(Data1Text)
+
+    ' Run when the actual text changes, even if the new text has the same length.
+    if Data1Text <> LastText then
+
+        if CharacterCount <= 50 then
+            BoxWidth = 500
+            BoxHeight = 200
+        else
+            BoxWidth = 700
+            BoxHeight = 300
+        end if
+
+        System.SendCommand(TextPath & "*GEOM*TBOX*WIDTH SET " & BoxWidth)
+        System.SendCommand(TextPath & "*GEOM*TBOX*HEIGHT SET " & BoxHeight)
+
+        Println "DATA1 characters: " & CharacterCount
+        Println "Text box width: " & BoxWidth
+        Println "Text box height: " & BoxHeight
+
+        LastText = Data1Text
+
+    end if
+
+end sub
